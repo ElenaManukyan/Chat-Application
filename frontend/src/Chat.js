55f543b2f3125d7f37289c35349de051e8a2c312
Elena <utv.header@gmail.com>
2024-09-21 15:23:25 +0300

    Initialization

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,21 +1,56 @@
-import React, { useEffect } from 'react';  
-import { useNavigate } from 'react-router-dom';  
-import { useLocation } from 'react-router-dom';  
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchChannels, fetchMessages } from './store/chatSlice';
+import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';  
+// import { useLocation } from 'react-router-dom';  
 
-const Chat = () => {  
-    const navigate = useNavigate();  
-    // const location = useLocation();  
+const Chat = () => {
+    const dispatch = useDispatch();
+    const channels = useSelector((state) => state.chat.channels);
+    const messages = useSelector((state) => state.chat.messages);
+    const chatStatus = useSelector((state) => state.chat.status);
+    const error = useSelector((state) => state.chat.error);
 
-    useEffect(() => {  
-        const token = localStorage.getItem('token');  
-        if (!token) {  
-            navigate('/login');
-        }  
-    }, [navigate]);  
+   useEffect(() => {
+    if (chatStatus === 'idle') {
+        dispatch(fetchChannels());
+        dispatch(fetchMessages());
+    }
+   }, [chatStatus, dispatch]);
+
+   if (chatStatus === 'loading') {
+    return <div>Загрузка...</div>
+   }
+
+   if (chatStatus === 'failed') {
+    return <div>Произошла ошибка: {error}</div>
+   }
+   
+   const navigate = useNavigate();
+   const handleLoginClick = () => {
+        console.log('Button clicked!');
+        navigate('/login');
+    };
 
     return (  
         <div>  
             <h2>Чат</h2>
+            <div>
+                <h3>Список каналов</h3>
+                {channels.map((channel) => (
+                    <div key={channel.id}>{channel.name}</div>
+                ))}
+            </div>
+            <div>
+                <h3>Сообщения</h3>
+                {messages.map((message) => (
+                    <div key={message.id}>
+                        <strong>{message.username}</strong> {message.body}
+                    </div>
+                ))}
+            </div>
+            <button type="button" onClick={handleLoginClick}>Перейти на страницу логина</button>
         </div>  
     );  
 };  
---
+++ frontend/src/store/chatSlice.js
@@ -0,0 +1,73 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+export const fetchChannels = createAsyncThunk(
+    'chat/fetchChannels',
+    async () => {
+        const token = localStorage.getItem('token');
+        // Get channels
+        const response = await axios.get('/api/v1/channels', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }).then((response) => {
+            console.log(response.data); // =>[{ id: '1', name: 'general', removable: false }, ...]
+          });
+          return response.data;
+    },
+);
+
+export const fetchMessages = createAsyncThunk(
+    'chat/fetchMessages',
+    async () => {
+        const token = localStorage.getItem('token');
+        // Get messages
+        const response = await axios.get('/api/v1/messages', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }).then((response) => {
+            console.log(response.data); // =>[{ id: '1', body: 'text message', channelId: '1', username: 'admin }, ...]
+          });
+          return response.data;
+    },
+);
+
+const chatSlice = createSlice({
+    name: 'chat',
+    initialState: {
+        channels: [],
+        messages: [],
+        channelsStatus: 'idle',
+        messagesStatus: 'idle',
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchChannels.pending, (state) => {
+                state.channelsStatus = 'loading';
+            })
+            .addCase(fetchChannels.fulfilled, (state, action) => {
+                state.channelsStatus = 'succeeded';
+                state.channels = action.payload;
+            })
+            .addCase(fetchChannels.rejected, (state, action) => {
+                state.channelsStatus = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(fetchMessages.pending, (state) => {
+                state.messagesStatus = 'loading';
+            })
+            .addCase(fetchMessages.fulfilled, (state, action) => {
+                state.messagesStatus = 'succeeded';
+                state.messages = action.payload;
+            })
+            .addCase(fetchMessages.rejected, (state, action) => {
+                state.messagesStatus = 'failed';
+                state.error = action.error.message;
+            })
+    },
+});
+
+export default chatSlice.reducer;
