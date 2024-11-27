ca20c61c3f1a30cde503051878a5921e0650b3cd
Elena <utv.header@gmail.com>
2024-11-27 19:02:45 +0300

    delete comment

--- frontend/src/Pages/Chat.js
+++ frontend/src/Pages/Chat.js
@@ -112,13 +112,6 @@ const Chat = () => {
     }
   };
 
-  // ЛОАДЕР ЗДЕСЬ!!
-  /*
-  if (status === 'loading') {
-    return <Spinner animation="border" />;
-  }
-    */
-
   if (status === 'failed') {
     return (
       <Alert variant="danger">
