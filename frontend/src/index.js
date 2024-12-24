e3c3326a6775379eb45b19fb68711e0926ddcd36
Elena <139575653+ElenaManukyan@users.noreply.github.com>
2024-12-24 22:11:39 +0300

    Update index.js. Delete comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import initializeApp from './initializeApp';
 
-/*
-const rollbarConfig = {
-  accessToken: '5c1bb74732e54c9a9054a8c03f8aaa96',
-  environment: 'testenv',
-};
-*/
-
 const app = initializeApp();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
